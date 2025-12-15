1883e8b8d55a2a4990dbe5e8c4903155b269a5a9
yigit-ozcan <oozcan.yigitt@gmail.com>
2025-12-16 02:03:52 +0300

    wrapped app with Theme and Language providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import { LanguageProvider } from './context/LanguageContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <ThemeProvider>
+    <LanguageProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </LanguageProvider>
+  </ThemeProvider>
+  
 )
