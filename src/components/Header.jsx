541ce410d6f206f7e40f273f557916a5cb764d89
yigit-ozcan <oozcan.yigitt@gmail.com>
2025-12-16 03:26:05 +0300

    theme and language toggle buttons in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import { LanguageContext } from "../context/LanguageContext";
+
 const Header = () => {
-  return <header>HEADER ÇALIŞIYOR</header>;
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const { lang, toggleLang } = useContext(LanguageContext);
+  return (
+    <>
+      <header>
+        HEADER ÇALIŞIYOR <br />
+        Theme: {theme}
+        Lang: {lang}
+      </header>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <button onClick={toggleLang}>Toggle Lang</button>
+    </>
+  );
 };
 
 export default Header;
